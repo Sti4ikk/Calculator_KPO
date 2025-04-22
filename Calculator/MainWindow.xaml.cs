3a82ff24037aaf311cfbdac380ca9f0bc4635589
nelipovi4 <zhenya.nelipovich@gmail.com>
2025-04-22 12:35:36 +0300

    ПАТТЕРН КОМАНДА

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,94 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 namespace Calculator
 {
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    // Интерфейс команды
+    public interface ICommand
     {
-      InitializeComponent();
+        void Execute(Context context);
+    }
 
-    foreach (UIElement el in MainRoot.Children)
-      {
-        if (el is Button)
-        {
-        ((Button)el).Click += Button_Click;
-        }
-        }
-     }
+    // Контекст для передачи данных
+    public class Context
+    {
+        public Label ResultLabel { get; set; }
+        public string Input { get; set; }
+    }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+    // Конкретные команды
+    public class ClearEntryCommand : ICommand
+    {
+        public void Execute(Context context)
         {
-            string str = (string)((Button)e.OriginalSource).Content;
-            List<string> list_methods = new List<string> { "+", "-", "*", "/", "C", "^2", "CE", "="};
-            switch (str)
+            context.ResultLabel.Text = "";
+        }
+    }
+
+    public class CalculateCommand : ICommand
+    {
+        public void Execute(Context context)
+        {
+            try
             {
-                case "CE":
-                    ResultLabel.Text = "";
-                    break;
-                case "=":
-                    string expression = ResultLabel.Text.Replace(",", "."); // Делаем это для устранения ошибок при подсчете
-                    string value = new DataTable().Compute(expression, null).ToString();
-                    ResultLabel.Text = value;
-                    break;
-                case "C":
-                    if (ResultLabel.Text.Length > 0)
-                    {
-                        ResultLabel.Text = ResultLabel.Text.Substring(0, ResultLabel.Text.Length - 1);
-                    }
-                    break;
-                case "^2":
-                    double number = double.Parse(ResultLabel.Text);
-                    ResultLabel.Text = Math.Pow(number, 2).ToString();
-                    break;
-                default:
-                    char lastChar = ResultLabel.Text.LastOrDefault();
-                    if (list_methods.Contains(lastChar.ToString()) && list_methods.Contains(str)) // Делаем проверку на повтор символа
-                    {
-                        //pass
-                    }
-                    else
-                    {
-                        ResultLabel.Text += str;
-                    }
-                    break;
+                string expression = context.ResultLabel.Text.Replace(",", ".");
+                string value = new DataTable().Compute(expression, null).ToString();
+                context.ResultLabel.Text = value;
             }
+            catch
+            {
+                context.ResultLabel.Text = "Error";
+            }
+        }
+    }
 
-
-
-
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+    public class DeleteLastCharCommand : ICommand
+    {
+        public void Execute(Context context)
         {
-            double normalSize = 18; // Обычный размер шрифта
-            double largeSize = 28;  // Увеличенный размер шрифта
+            if (context.ResultLabel.Text.Length > 0)
+                context.ResultLabel.Text = context.ResultLabel.Text.Substring(0, context.ResultLabel.Text.Length - 1);
+        }
+    }
+
+    public class SquareCommand : ICommand
+    {
+        public void Execute(Context context)
+        {
+            if (double.TryParse(context.ResultLabel.Text, out double number))
+                context.ResultLabel.Text = Math.Pow(number, 2).ToString();
+            else
+                context.ResultLabel.Text = "Error";
+        }
+    }
+
+    public class AppendTextCommand : ICommand
+    {
+        private readonly string _text;
+        private readonly List<string> _operators = new() { "+", "-", "*", "/", "C", "^2", "CE", "=" };
+
+        public AppendTextCommand(string text)
+        {
+            _text = text;
+        }
+
+        public void Execute(Context context)
+        {
+            string lastChar = context.ResultLabel.Text.Length > 0
+                ? context.ResultLabel.Text[^1].ToString()
+                : "";
+
+            if (_operators.Contains(lastChar) && _operators.Contains(_text))
+                return;
+
+            context.ResultLabel.Text += _text;
+        }
+    }
+
+    // Фабрика для создания нужной команды
+    public static class CommandFactory
+    {
+        public static ICommand CreateCommand(string input) => input switch
+        {
+            "CE" => new ClearEntryCommand(),
+            "=" => new CalculateCommand(),
+            "C" => new DeleteLastCharCommand(),
+            "^2" => new SquareCommand(),
+            _ => new AppendTextCommand(input)
+        };
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
 
             foreach (UIElement el in MainRoot.Children)
             {
-                if (el is Label lbl)
+                if (el is Button button)
                 {
-                    lbl.FontSize = isLargeFont ? normalSize : largeSize;
-                }
-                else if (el is Button btn)
-                {
-                    btn.FontSize = isLargeFont ? normalSize : largeSize;
+                    button.Click += Button_Click;
                 }
             }
+        }
 
-            isLargeFont = !isLargeFont; // Переключаем флаг
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string input = button.Content.ToString();
+                var context = new Context { ResultLabel = ResultLabel, Input = input };
+
+                // Создание и выполнение команды
+                ICommand command = CommandFactory.CreateCommand(input);
+                command.Execute(context);
+            }
         }
     }
 }
